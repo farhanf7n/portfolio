214739034879061ba5a8e76370446606ce1ec285
Farhan Ahmed <farhanf7n@gmail.com>
2024-11-05 23:31:10 +0500

    Fixed outdated stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,4 +11,11 @@ export default defineConfig({
       },
     }),
   ],
+  vite: {
+    css: {
+      postcss: {
+        plugins: []
+      }
+    }
+  }
 });
