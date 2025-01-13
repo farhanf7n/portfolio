d0039840cd2e786aeb8f2415b2ea4aa0d64ebcb6
Farhan Ahmed <farhanf7n@gmail.com>
2025-01-14 00:10:38 +0500

    Added Gallery section

--- src/components/threeImages.tsx
+++ src/components/threeImages.tsx
@@ -1,8 +1,9 @@
 'use client';
-import { clsx } from 'clsx';
 import type { ComponentProps } from 'react';
-import { motion, useScroll, useTransform } from 'framer-motion';
 import { useRef } from 'react';
+import { clsx } from 'clsx';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import Image from 'next/image';
 
 export function ThreeImages({
   className,
@@ -14,55 +15,63 @@ export function ThreeImages({
     offset: ['start end', 'end start'],
   });
 
-  const rotate1 = useTransform(scrollYProgress, [0, 1], [8, 0]);
-  const rotate2 = useTransform(scrollYProgress, [0, 1], [12, 0]);
-  const rotate3 = useTransform(scrollYProgress, [0, 1], [-8, 0]);
-  const rotate4 = useTransform(scrollYProgress, [0, 1], [-12, 0]);
+  const rotate1 = useTransform(scrollYProgress, [0, 1], [-4, 0]);
+  const rotate2 = useTransform(scrollYProgress, [0, 1], [-4, 0]);
+  const rotate3 = useTransform(scrollYProgress, [0, 1], [4, 0]);
+  const rotate4 = useTransform(scrollYProgress, [0, 1], [4, 0]);
 
   return (
     <section className={clsx(className, 'content my-14')} {...props} ref={ref}>
-      <div className="flex flex-wrap gap-2">
+      <div className="flex gap-4">
         <motion.div
           style={{ rotate: rotate1 }}
-          className="flex flex-col p-3 border-1 border-gray-200 rounded-md w-[260px] h-[285px] overflow-hidden"
+          className="flex min-w-[180px] flex-col overflow-hidden rounded-md border border-gray-200/50 p-3 sm:min-w-[220px] md:min-w-[260px] dark:border-white/30"
         >
-          <motion.img
-            className="object-cover w-full h-full rounded-md"
+          <Image
+            className="size-full rounded-md object-cover"
             src="/gallery1.jpg"
             alt="About 1"
+            width={260}
+            height={285}
           />
         </motion.div>
 
         <motion.div
           style={{ rotate: rotate2 }}
-          className="flex flex-col p-3 border-1 border-gray-200 rounded-md w-[260px] h-[285px]"
+          className="flex min-w-[180px] flex-col overflow-hidden rounded-md border border-gray-200/50 p-3 sm:min-w-[220px] md:min-w-[260px] dark:border-white/30"
         >
-          <img
-            className="object-cover w-full h-full rounded-md"
+          <Image
+            className="size-full rounded-md object-cover"
             src="/gallery2.jpg"
             alt="About 2"
+            width={260}
+            height={285}
           />
         </motion.div>
 
         <motion.div
           style={{ rotate: rotate3 }}
-          className="flex flex-col p-3 border-1 border-gray-200 rounded-md w-[260px] h-[285px]"
+          className="flex min-w-[180px] flex-col overflow-hidden rounded-md border border-gray-200/50 p-3 sm:min-w-[220px] md:min-w-[260px] dark:border-white/30"
         >
-          <img
-            className="object-cover w-full h-full rounded-md"
+          <Image
+            className="size-full rounded-md object-cover"
             src="/gallery3.jpg"
             alt="About 3"
+            width={260}
+            height={285}
           />
         </motion.div>
 
         <motion.div
           style={{ rotate: rotate4 }}
-          className="flex flex-col p-3 border-1 border-gray-200 rounded-md w-[260px] h-[285px]"
+          className="flex min-w-[180px] flex-col overflow-hidden rounded-md border border-gray-200/50 p-3 sm:min-w-[220px] md:min-w-[260px] dark:border-white/30"
         >
-          <img
-            className="object-cover w-full h-full rounded-md"
+          <Image
+            className="size-full rounded-md object-cover"
             src="/gallery4.jpg"
             alt="About 4"
+            width={260}
+            height={285}
           />
         </motion.div>
       </div>
