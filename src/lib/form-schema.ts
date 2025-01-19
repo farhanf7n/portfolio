02d7839c8ca3aacd351aa6def269f27346471042
Farhan Ahmed <farhanf7n@gmail.com>
2025-01-19 08:56:08 +0500

    Updated form scheme

--- src/lib/form-schema.ts
+++ src/lib/form-schema.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 
 export const formSchema = z.object({
+  name: z.string().min(1, { message: 'Name is required' }),
   email: z
     .string()
     .min(1, { message: 'Email is required' })
