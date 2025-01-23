3ff49c410a5f7a59c0fec410de4f7ce4759b3956
Farhan Ahmed <farhanf7n@gmail.com>
2025-01-24 02:37:32 +0500

    Updated navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { motion } from 'framer-motion';
 import Link from 'next/link';
 
@@ -143,10 +142,10 @@ export const Navbar = () => {
             </Link>
           </HoverTooltip>
 
-          <HoverTooltip content="Services">
+          <HoverTooltip content="Contact">
             <Link
               className="hover:text-foreground focusable relative rounded-md bg-transparent p-1.5 px-4 py-2 transition hover:bg-gray-500/10 dark:hover:bg-gray-400/20"
-              href="/services"
+              href="#contact"
             >
               <svg
                 className="text-black dark:text-white"
@@ -158,18 +157,33 @@ export const Navbar = () => {
                 fill="none"
               >
                 <path
-                  d="M9 14.5H13M9 9.5H17"
+                  d="M12 22C7.99306 22 5.98959 22 4.7448 20.682C3.5 19.364 3.5 17.2426 3.5 13C3.5 8.75736 3.5 6.63604 4.7448 5.31802C5.98959 4 7.99306 4 12 4C16.0069 4 18.0104 4 19.2552 5.31802C20.5 6.63604 20.5 8.75736 20.5 13C20.5 17.2426 20.5 19.364 19.2552 20.682C18.0104 22 16.0069 22 12 22Z"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M8 4V2"
                   stroke="currentColor"
                   strokeWidth="1.5"
                   strokeLinecap="round"
                 />
                 <path
-                  d="M4 10C4 6.22876 4 4.34315 5.17157 3.17157C6.34315 2 8.22876 2 12 2H13.5C17.2712 2 19.1569 2 20.3284 3.17157C21.5 4.34315 21.5 6.22876 21.5 10V14C21.5 17.7712 21.5 19.6569 20.3284 20.8284C19.1569 22 17.2712 22 13.5 22H12C8.22876 22 6.34315 22 5.17157 20.8284C4 19.6569 4 17.7712 4 14V10Z"
+                  d="M16 4V2"
                   stroke="currentColor"
                   strokeWidth="1.5"
+                  strokeLinecap="round"
                 />
                 <path
-                  d="M5 6L2.5 6M5 12L2.5 12M5 18H2.5"
+                  d="M14.018 9.49261C14.018 10.5972 13.1226 11.4926 12.0181 11.4926C10.9135 11.4926 10.0181 10.5972 10.0181 9.49261C10.0181 8.38808 10.9135 7.49268 12.0181 7.49268C13.1226 7.49268 14.018 8.38808 14.018 9.49261Z"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M8.06298 16.7161C9.12133 15.0868 10.802 14.4762 12.0181 14.4774C13.2341 14.4787 14.8656 15.0868 15.9239 16.7161C15.9923 16.8215 16.0112 16.9512 15.9494 17.0607C15.7019 17.4996 14.9334 18.3705 14.3784 18.4296C13.7406 18.4974 12.0723 18.5069 12.0194 18.5072C11.9664 18.5069 10.2466 18.4974 9.60851 18.4296C9.05345 18.3705 8.28496 17.4996 8.03745 17.0607C7.9757 16.9512 7.99456 16.8215 8.06298 16.7161Z"
                   stroke="currentColor"
                   strokeWidth="1.5"
                   strokeLinecap="round"
