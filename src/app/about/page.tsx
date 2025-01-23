07ac3bd23aa1a587fba1bf432c5bb42a84f6d6a1
Farhan Ahmed <farhanf7n@gmail.com>
2025-01-24 02:40:24 +0500

    Added Aura

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,7 @@
 'use client';
+import styles from '../layout.module.css';
 
+import clsx from 'clsx';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 
@@ -31,9 +33,29 @@ const tools = [
 export default function Page() {
   return (
     <>
+      {/* Aura Effect */}
       <div className="pointer-events-none absolute top-0 h-72 w-full overflow-hidden md:h-80 lg:h-96">
         <div className="content relative h-full">
-          {/* Background aura effect */}
+          <div
+            className={clsx(
+              styles.auraMask,
+              'absolute left-[-100%] z-negative h-full w-[300%] overflow-hidden opacity-50 [--aura-rainbow-offset:-20%] dark:opacity-30 sm:[--aura-rainbow-offset:-15%] md:left-[-150%] md:w-[400%] md:[--aura-offset:-10%]'
+            )}
+          >
+            <div
+              className={clsx(
+                styles.auraRaysMask,
+                'absolute inset-0 flex items-center'
+              )}
+            >
+              <div
+                className={clsx(
+                  styles.auraRainbowGradient,
+                  'absolute left-[-10%] aspect-square w-[120%]'
+                )}
+              />
+            </div>
+          </div>
         </div>
       </div>
       <div className="container flex flex-col items-center">
