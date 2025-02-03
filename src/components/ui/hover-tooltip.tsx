c2f98785cc7d3341b65a0e631f3eea69b9a517d9
Farhan Ahmed <farhanf7n@gmail.com>
2025-02-03 05:23:40 +0500

    Refactored code

--- src/components/ui/hover-tooltip.tsx
+++ src/components/ui/hover-tooltip.tsx
@@ -1,12 +1,13 @@
 'use client';
 
+import { ReactNode } from 'react';
+
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
 } from '@/components/ui/tooltip';
-import { ReactNode } from 'react';
 
 interface HoverTooltipProps {
   children: ReactNode;
