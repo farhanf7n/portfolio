72bbfea2fd5d672ba2b9bc70a198990a25e33e2f
Farhan Ahmed <farhanf7n@gmail.com>
2025-02-06 03:04:55 +0500

    Added Projects component

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,150 @@
+'use client';
+import styles from '../layout.module.css';
+
+import clsx from 'clsx';
+import { motion } from 'framer-motion';
+
+import { Footer } from '@/components/footer';
+import BlurFade from '@/components/magicui/blur-fade';
+import { Navbar } from '@/components/navbar';
+import { ProjectCard } from '@/components/project-card';
+import { SectionDivider } from '@/components/section-divider';
+import { ThemeToggle } from '@/components/theme-toggle';
+import { projectsData, uiWork } from '@/lib/data';
+import { Contact } from '@/components/sections/contact';
+
+const BLUR_FADE_DELAY = 0.04;
+
+export default function Page() {
+  return (
+    <>
+      {/* Aura Effect */}
+      <div className="pointer-events-none absolute top-0 h-72 w-full overflow-hidden md:h-80 lg:h-96">
+        <div className="content relative h-full">
+          <div
+            className={clsx(
+              styles.auraMask,
+              'absolute left-[-100%] z-negative h-full w-[300%] overflow-hidden opacity-50 [--aura-rainbow-offset:-20%] dark:opacity-30 sm:[--aura-rainbow-offset:-15%] md:left-[-150%] md:w-[400%] md:[--aura-offset:-10%]'
+            )}
+          >
+            <div
+              className={clsx(
+                styles.auraRaysMask,
+                'absolute inset-0 flex items-center'
+              )}
+            >
+              <div
+                className={clsx(
+                  styles.auraRainbowGradient,
+                  'absolute left-[-10%] aspect-square w-[120%]'
+                )}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container flex flex-col items-center">
+        <Navbar />
+
+        <div className="mt-10 flex scroll-mt-96 flex-col gap-4 sm:mt-10">
+          <motion.h1
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            className="text-left text-3xl font-bold leading-tight tracking-tighter sm:text-4xl"
+          >
+            What I&apos;ve{' '}
+            <span className="font-bold dark:text-[#e8ff85]">Built</span>
+          </motion.h1>
+          <motion.p
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.1,
+            }}
+            className="text-muted-foreground max-w-[56ch] text-left leading-loose"
+          >
+            I have worked on a variety of projects over the years; some of them
+            as a hobby, some as a proof of concept and others to solve my own
+            pain points. Here are some of the projects that I have worked on.
+          </motion.p>
+        </div>
+
+        <SectionDivider />
+
+        <div className="flex w-full max-w-xl flex-col gap-4">
+          <motion.h1
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            className="mb-4 text-left text-3xl font-bold leading-tight tracking-tighter sm:text-4xl"
+          >
+            <span className="font-bold dark:text-[#e8ff85]">Projects</span>
+          </motion.h1>
+
+          {/* All Projects */}
+          <BlurFade delay={BLUR_FADE_DELAY}>
+            <div className="w-full space-y-12">
+              <div className="mx-auto mt-2 grid max-w-[800px] grid-cols-1 gap-4 sm:grid-cols-2">
+                {projectsData.map((projectsData, id) => (
+                  <BlurFade
+                    key={projectsData.title}
+                    delay={BLUR_FADE_DELAY * 12 + id * 0.05}
+                  >
+                    <ProjectCard
+                      href={projectsData.href}
+                      key={projectsData.title}
+                      title={projectsData.title}
+                      description={projectsData.description}
+                      tags={projectsData.technologies}
+                      image={projectsData.image}
+                      video={projectsData.video}
+                      links={projectsData.links}
+                    />
+                  </BlurFade>
+                ))}
+              </div>
+            </div>
+          </BlurFade>
+
+          <SectionDivider />
+
+          <motion.h1
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            className="mb-4 text-left text-3xl font-bold leading-tight tracking-tighter sm:text-4xl"
+          >
+            Ui <span className="font-bold dark:text-[#e8ff85]">Designs</span>
+          </motion.h1>
+          {/* UI Designs */}
+          <BlurFade delay={BLUR_FADE_DELAY}>
+            <div className="w-full space-y-12">
+              <div className="mx-auto mt-2 grid max-w-[800px] grid-cols-1 gap-3 sm:grid-cols-2">
+                {uiWork.map((project, id) => (
+                  <BlurFade
+                    key={project.title}
+                    delay={BLUR_FADE_DELAY * 12 + id * 0.05}
+                  >
+                    <ProjectCard
+                      href={project.href}
+                      key={project.title}
+                      title={project.title}
+                      description={project.description}
+                      tags={project.technologies}
+                      image={project.image}
+                      video={project.video}
+                      links={project.links}
+                    />
+                  </BlurFade>
+                ))}
+              </div>
+            </div>
+          </BlurFade>
+        </div>
+
+        <Contact />
+
+        <Footer />
+      </div>
+      <ThemeToggle className="fixed bottom-5 right-5 hidden sm:bottom-8 sm:right-8 sm:flex" />
+    </>
+  );
+}
---
+++ src/pages/Projects.tsx
@@ -0,0 +1,51 @@
+import { motion } from 'framer-motion';
+
+import { Footer } from '@/components/footer';
+import { Navbar } from '@/components/navbar';
+import { Skills } from '@/components/skills';
+import { ThemeToggle } from '@/components/theme-toggle';
+
+export default function AboutPage() {
+  return (
+    <>
+      <div className="pointer-events-none absolute top-0 h-72 w-full overflow-hidden md:h-80 lg:h-96">
+        <div className="content relative h-full"></div>
+      </div>
+      <div className="container flex flex-col items-center">
+        <Navbar />
+        <main className="flex flex-col items-center px-4">
+          <motion.section
+            className="my-10 flex w-full max-w-4xl flex-col items-center"
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.175 }}
+          >
+            <div className="-mt-5 max-w-2xl text-center leading-7">
+              <p className="mb-4">
+                I&apos;m Farhan Ahmed, I&apos;m a Freelance Developer & Designer
+                from Pakistan. I&apos;ve been learning programming for over 3
+                years. After I started high school, I decided to pursue my
+                passion for programming. My favorite part of programming is the
+                problem-solving aspect. I love the feeling of finally figuring
+                out a solution to a problem. I&apos;ve had the opportunity to
+                work on several exciting projects during my learning journey,
+                ranging from personal projects to collaborative ventures. My
+                core stack is React, Next.js, TypeScript, and Tailwind CSS. I am
+                also familiar with Nest.js and Prisma. I am always looking to
+                learn new technologies.
+              </p>
+              <p>
+                I&apos;m open to Job opportunities where I can contribute, learn
+                and grow. If you have a good opportunity that matches my skills
+                and experience then don&apos;t hesitate to contact me.
+              </p>
+            </div>
+            <Skills />
+          </motion.section>
+        </main>
+        <Footer />
+      </div>
+      <ThemeToggle className="fixed bottom-5 right-5 hidden sm:bottom-8 sm:right-8 sm:flex" />
+    </>
+  );
+}
