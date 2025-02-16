15943ab88e9a1b27a5063e93f7f7b061dc8fde3a
Farhan Ahmed <farhanf7n@gmail.com>
2025-02-17 03:19:04 +0500

    Updated styling

--- src/components/section-divider.tsx
+++ src/components/section-divider.tsx
@@ -5,10 +5,19 @@ import { motion } from 'framer-motion';
 export const SectionDivider = () => {
   return (
     <motion.div
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.125 }}
-      className="bg-muted m-auto my-5 h-16 w-1 rounded-full"
+      initial={{ opacity: 0, y: 100, height: 0 }}
+      animate={{ opacity: 1, y: 0, height: 64 }}
+      transition={{
+        delay: 0.125,
+        height: {
+          duration: 0.5,
+          ease: 'easeOut',
+        },
+      }}
+      style={{
+        backgroundImage: 'linear-gradient(to top, #e8ff85, transparent)',
+      }}
+      className="m-auto my-5 w-[1px] rounded-full"
     ></motion.div>
   );
 };
