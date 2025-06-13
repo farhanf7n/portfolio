a3068a9f4c216fffc90e3575e6fd2c44d0793039
Farhan Ahmed <farhanf7n@gmail.com>
2025-06-14 03:04:15 +0500

    Updated name font

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -52,7 +52,7 @@ export function Introduction({
           }}
           className="flex flex-col"
         >
-          <h1 className="text-2xl font-semibold text-gray-800 dark:text-white">
+          <h1 className="font-heading text-3xl font-semibold text-gray-800 dark:text-white">
             Farhan Ahmed
           </h1>
           <p className="dark:text-gray-450 text-sm text-gray-400 sm:text-base">
